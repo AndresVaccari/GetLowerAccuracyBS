11db2d5af1ffbebe1a8a603050232a20bf9cbcf6
Andres Vaccari <andresvaccari34@gmail.com>
2023-06-10 22:55:02 -0300

    Added logo to main page

--- app/components/Logo.jsx
+++ app/components/Logo.jsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 export default function Logo({ list }) {
   return (
     <>
       {!list.length > 0 && (
-        <div className="flex flex-col gap-2 h-20 items-center justify-center">
+        <div className="flex flex-col gap-2 h-1/6 items-center justify-center">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={100}
+            height={100}
+            className="rounded-xl"
+          />
           <span className="text-white">Lower Accuracy BS</span>
           <span className="text-white">
             Get the lower accuracy Beat Saber songs on ScoreSaber
