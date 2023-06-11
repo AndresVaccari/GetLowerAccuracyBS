5bcc1dc2fb522e34f6b7dde25e9005a2c8c1c6e6
Andres Vaccari <andresvaccari34@gmail.com>
2023-06-10 22:38:19 -0300

    Help image and form validation

--- app/components/InputForm.jsx
+++ app/components/InputForm.jsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+import { useState } from "react";
 import { AiFillStar } from "react-icons/ai";
+import { BiHelpCircle } from "react-icons/bi";
 
 export function InputForm({
   setUserId,
@@ -10,16 +13,53 @@ export function InputForm({
   setQuantity,
   setMinStars,
   setMaxStars,
+  fetchError,
 }) {
-  return (
-    <div className="flex flex-col gap-2 h-1/3 items-center justify-center">
-      <input
-        type="text"
-        className="border-2 border-gray-300 rounded-md p-2 text-black"
-        placeholder="ScoreSaber ID"
-        onChange={(e) => setUserId(e.target.value)}
-      />
+  const [showHelp, setShowHelp] = useState(false);
 
+  const toggleHelp = () => {
+    setShowHelp(!showHelp);
+  };
+
+  return (
+    <form
+      className="flex flex-col gap-2 h-1/3 w-1/4 items-center justify-center"
+      onSubmit={(e) => getList(e)}
+    >
+      <div className="flex w-full justify-center items-center gap-2">
+        <input
+          type="text"
+          className={`border-2 border-gray-300 w-full rounded-md p-2 text-black ${
+            fetchError && "border-red-500"
+          }`}
+          placeholder="ScoreSaber ID"
+          required
+          pattern="^[0-9]+$"
+          onChange={(e) => setUserId(e.target.value)}
+        />
+        <button
+          className="border-2 border-gray-300 rounded-md p-1 h-full"
+          type="button"
+          onClick={toggleHelp}
+        >
+          <BiHelpCircle size={25} />
+        </button>
+      </div>
+      {showHelp && (
+        <div className="flex flex-col gap-2 w-full items-center justify-center">
+          <Image
+            src="/helpImage2.png"
+            alt="ScoreSaber ID"
+            width={500}
+            height={200}
+            className="rounded-xl"
+          />
+          <span className="text-white text-xs">
+            Your ScoreSaber ID can be found in the URL of your ScoreSaber
+            profile.
+          </span>
+        </div>
+      )}
       <select
         onChange={(e) => setQuantity(e.target.value)}
         className="border-2 border-gray-300 rounded-md p-2 text-black w-full"
@@ -61,7 +101,7 @@ export function InputForm({
 
       <button
         className="border-2 border-gray-300 rounded-md p-2 w-full"
-        onClick={getList}
+        type="submit"
       >
         Get List
       </button>
@@ -69,10 +109,11 @@ export function InputForm({
         <button
           className="border-2 border-gray-300 rounded-md p-2 w-full"
           onClick={generateDowloadableFile}
+          type="button"
         >
           Generate File
         </button>
       )}
-    </div>
+    </form>
   );
 }
